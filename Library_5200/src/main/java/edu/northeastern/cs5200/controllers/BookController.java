8ddf0aa5718bb186e5e638897f1a6135e7cd8f03
Alice Tilles <tilles.al@husky.neu.edu>
2020-04-21 09:12:23 -0400

    find book by title

--- Library_5200/src/main/java/edu/northeastern/cs5200/controllers/BookController.java
+++ Library_5200/src/main/java/edu/northeastern/cs5200/controllers/BookController.java
@@ -5,11 +5,7 @@ import edu.northeastern.cs5200.models.*;
 import edu.northeastern.cs5200.repositories.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,7 +28,9 @@ public class BookController {
     }
 
 
-//    @GetMapping("api/books/title/{bookTitle}")
-//    public
+    @GetMapping("api/books/title/{bookTitle}")
+    public Book findBookByTitle(@PathVariable String bookTitle) {
+        return libraryDao.findBookByTitle(bookTitle);
+    }
 
 }
